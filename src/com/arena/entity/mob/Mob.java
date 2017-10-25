35bc7f0a280e6ae3238055e808e17afb2dae62a2
Gilad Ramati <gsramati@gmail.com>
2017-10-26 00:05:07 +0300

    44 done

--- src/com/arena/entity/mob/Mob.java
+++ src/com/arena/entity/mob/Mob.java
@@ -3,14 +3,32 @@ package com.arena.entity.mob;
 import com.arena.entity.Entity;
 import com.arena.graphics.Sprite;
 
+import static com.arena.entity.Entity.Direction.*;
+
 public abstract class Mob extends Entity {
 
     protected Sprite sprite;
-    protected int dir = 0; //0 - north 1 - east 2 - south 3 - west
+    protected Direction dir = UP;
     protected boolean moving = false;
 
-    public void move(){
-
+    public void move(int xChange, int yChange){
+        //
+        if(xChange > 0){
+            dir = RIGHT;
+        }
+        if(xChange < 0){
+            dir = LEFT;
+        }
+        if(yChange > 0){
+            dir = DOWN;
+        }
+        if(yChange < 0){
+            dir = UP;
+        }
+        if (!collision()) {
+            x += xChange;
+            y += yChange;
+        }
     }
 
     public void update(){
